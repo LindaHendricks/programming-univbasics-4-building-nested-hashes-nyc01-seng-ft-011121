d9a2ded90df1124f49fb2b673d34daab782008aa
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-20 23:59:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,6 +37,9 @@ def monopoly_with_fourth_tier
  hash = monopoly_with_third_tier
   if monopoly_with_third_tier.include? :railroads
     hash[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+    hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 200 
+    hash[:railroads][:names][:b_and_o_railroad][:mortgage_value]= 400 
+    hash[:railroads][:names][:shortline_railroad][:mortgage_value] = 800
   end
   hash
 end
