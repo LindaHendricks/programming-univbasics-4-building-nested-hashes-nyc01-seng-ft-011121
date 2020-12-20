16fd1674f0b9a76cd05d86c8d155c1879028ccf4
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-20 23:35:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_second_tier
 
 def monopoly_with_third_tier 
   hash = monopoly_with_second_tier
-  if monopoly_with_second_tier.include ? [:railroads]
+  if monopoly_with_second_tier.include ? [:rent_in_dollars]
     hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
     hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
     hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
